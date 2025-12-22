043bb5cfd813fd54018b22df77da0ba405be39e8
Vinay <53383503+vikuviku6666@users.noreply.github.com>
2025-12-22 14:43:36 +0100

    basics

---
+++ tut-01/main.go
@@ -0,0 +1,10 @@
+package main
+
+// fmt or format package
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+	fmt.Print("This is some text")
+	fmt.Print("and this is really some text.")
+}
